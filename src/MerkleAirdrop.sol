ce7a1fd43aa83075c0755f7653e703e3da8a183e
breziokwu-dev <ba.eziokwu@gmail.com>
2026-09-05 15:03:04 +0100

    feat: Implemented the claim function

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {MerkleProof} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop is Ownable {
 
@@ -16,6 +17,12 @@ contract MerkleAirdrop is Ownable {
     error InvalidToken();
     error InvalidMerkleRoot();
     error InvalidExpiration();
+    error AirdropExpired();
+    error AlreadyClaimed();
+    error InvalidProof();
+    error InsufficientAirdropBalance();
+
+    event Claimed(address indexed account, uint256 amount);
 
     constructor(address _token, bytes32 _merkleRoot, uint256 _expiration) Ownable(msg.sender) {
         if (_token == address(0)) revert InvalidToken();
@@ -24,5 +31,21 @@ contract MerkleAirdrop is Ownable {
         token = _token;
         merkleRoot = _merkleRoot;
         expiration = _expiration;
+    }
+
+    function claim(uint256 amount, bytes32[] calldata proof) external {
+        if (block.timestamp >= expiration) revert AirdropExpired();
+        if (hasClaimed[msg.sender]) revert AlreadyClaimed();
+        if (!verifyProof(msg.sender, amount, proof)) revert InvalidProof();
+        uint256 balance = IERC20(token).balanceOf(address(this));
+        if (balance < amount) revert InsufficientAirdropBalance();
+        hasClaimed[msg.sender] = true;
+        IERC20(token).transfer(msg.sender, amount);
+        emit Claimed(msg.sender, amount);
+    }
+
+    function verifyProof(address account, uint256 amount, bytes32[] calldata proof) public view returns (bool) {
+        bytes32 leaf = keccak256(abi.encode(account, amount));
+        return MerkleProof.verify(proof, merkleRoot, leaf);
     }
 }
